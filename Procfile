11dba85651bc044e128a83f848bf12cc29b37fdd
thebookgy <apinut.o@appman.co.th>
2021-05-06 16:29:13 +0700

    edit script

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bin/start-nginx-solo && bash start.sh
+web: bash start.sh
--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@ set -e
 
 echo "starting vault..."
 
-
+bin/start-nginx 
+bundle exec unicorn -c config/unicorn.rb
 ./dev-config.sh > /tmp/dev.json
 # ./unsealer.sh &
 
